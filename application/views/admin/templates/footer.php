2b5c1eb1de3211a0d85c3832da56a84ac08c149e
Khoirony Arief <khoironyarief08@gmail.com>
2022-07-04 20:58:10 +0700

    update

--- application/views/admin/templates/footer.php
+++ application/views/admin/templates/footer.php
@@ -5,7 +5,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Khoironi Arief <?= date('Y'); ?></span>
+            <span>Copyright &copy; Maulana Rifqi <?= date('Y'); ?></span>
         </div>
     </div>
 </footer>
